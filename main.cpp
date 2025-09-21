eef7e421fc5c27b1860ac212fd3be77b0fbd2094
mayeshaaamaliha <mayeshamaliha728@gmail.com>
2025-09-21 15:43:26 +0600

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,7 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main(){
-
-    int n;
-    cin>>n;
-
-
-    int fact=1;
-
-    for(int i=1;i<=n;i++){
-        fact=fact*i;
-    }
-
-    cout<<fact<<endl;
-
-
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
 }
